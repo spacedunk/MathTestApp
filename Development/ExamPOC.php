da11501c63161b10bbe6c9b49799b065ee10a7e8
Spacedunk <spacedunk@gmail.com>
2016-04-14 08:26:00 -0400

    Added support for exporting the HTML file to a PDF and displying it to a screen for viewing and download

--- Development/ExamPOC.php
+++ Development/ExamPOC.php
@@ -3,21 +3,29 @@
 include "MathPOC_DB.php";
 include "TableRows.php";
 
-echo "<table class='table'>";
-echo "<tr><th>UserID</th><th>Firstname</th><th>Lastname</th></tr>";
 
-$connInfo = simplexml_load_file('ConnectionInfo.xml');
-
-$MathPOC = new MathPOC_DB($connInfo->Servername,$connInfo->DBName,$connInfo->UserName,$connInfo->Password);
-
-$results = $MathPOC->ExecuteQuery("SELECT UserID, First,Last from TestTable");
-
-foreach (new TableRows(new RecursiveArrayIterator($results)) as $key => $value) 
+class ExamPOC
 {
-	echo $value;
+	public static function GetUsers()
+	{
+		echo "<table class='table'>";
+		echo "<tr><th>UserID</th><th>Firstname</th><th>Lastname</th></tr>";
+
+		$connInfo = simplexml_load_file('ConnectionInfo.xml');
+
+		$MathPOC = new MathPOC_DB($connInfo->Servername,$connInfo->DBName,$connInfo->UserName,$connInfo->Password);
+
+		$results = $MathPOC->ExecuteQuery("SELECT UserID, First,Last from TestTable");
+
+		foreach (new TableRows(new RecursiveArrayIterator($results)) as $key => $value) 
+		{
+			echo $value;
+		}
+
+		echo "</table>";
+	}
 }
 
-echo "</table>";
-
+ExamPOC::GetUsers();
 
 ?>
